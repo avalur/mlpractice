2e085cebf98ae04a047aba938910953985c6f993
uvd174 <st075915@student.spbu.ru>
2022-03-16 15:02:40 +0300

    Add jupyter as a dependency. Some fixes to rnn_torch task.

--- mlpractice/rnn_torch/__init__.py
+++ mlpractice/rnn_torch/__init__.py
@@ -1,7 +1,9 @@
-r"""This time you'll understand thoroughly the insides of recurrent neural networks on a class of toy problems.
+r"""This time you'll understand thoroughly the insides of recurrent neural
+networks on a class of toy problems.
 
-Struggle to find a name for the variable? Let's see how you'll come up with a name for your son/daughter. 
-Surely no human has expertize over what is a good child name, so let us train RNN instead ;)
+Struggle to find a name for the variable? Let's see how you'll come up with
+a name for your son/daughter. Surely no human has expertize over what is
+a good child name, so let us train RNN instead ;)
 
 You should fill in the gaps in the given function templates.
 """
--- mlpractice/rnn_torch/rnn_torch.py
+++ mlpractice/rnn_torch/rnn_torch.py
@@ -1,3 +1,9 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from typing import Dict
+
+
 def make_tokens(input_text):
     r"""Computes all unique characters in the `input_text`.
 
@@ -22,38 +28,56 @@ def make_token_to_id(tokens):
     Parameters
     ----------
     tokens : list of str
+        List with all unique tokens.
 
     Returns
     -------
-    token_to_id : dict
-        Tokens to its identifier (index in tokens list)
+    token_to_id : dict of str
+        Tokens to its identifier (index in tokens list).
     """
     # TODO: make_token_to_id
     raise NotImplementedError('Not implemented!')
 
 
-import torch, torch.nn as nn
-import torch.nn.functional as F
-
 class CharRNNCell(nn.Module):
-    r"""
-    Implement vanilla rnn-cell as torch module
+    r"""Implement vanilla rnn-cell as torch module.
+
+    Attributes
+    ----------
+    num_units : ...
+        ...
+    embedding : ...
+        ...
+    rnn_update : ...
+        ...
+    rnn_to_logits : ...
+        ...
     """
-    # def __init__(self, num_tokens=len(tokens), embedding_size=16, rnn_num_units=64):
-    #     super(self.__class__, self).__init__()
-    #     self.num_units = rnn_num_units
-    #
-    #     self.embedding = nn.Embedding(num_tokens, embedding_size)
-    #     self.rnn_update = nn.Linear(embedding_size + rnn_num_units, rnn_num_units)
-    #     self.rnn_to_logits = nn.Linear(rnn_num_units, num_tokens)
+    def __init__(self, num_tokens, embedding_size=16, rnn_num_units=64):
+        super(self.__class__, self).__init__()
+        self.num_units = rnn_num_units
+
+        self.embedding = nn.Embedding(num_tokens, embedding_size)
+        self.rnn_update = nn.Linear(
+            embedding_size + rnn_num_units,
+            rnn_num_units,
+        )
+        self.rnn_to_logits = nn.Linear(rnn_num_units, num_tokens)
 
     def forward(self, x, h_prev):
-        r"""
-        This method computes h_next(x, h_prev) and log P(x_next | h_next)
+        r"""Compute h_next(x, h_prev) and log(P(x_next | h_next)).
         We'll call it repeatedly to produce the whole sequence.
 
-        :param x: batch of character ids, int64[batch_size]
-        :param h_prev: previous rnn hidden states, float32 matrix [batch, rnn_num_units]
+        Parameters
+        ----------
+        x : LongTensor, shape(batch_size)
+            Batch of character ids.
+        h_prev : FloatTensor, shape(batch, rnn_num_units)
+            Previous rnn hidden states.
+
+        Returns
+        -------
+
         """
         # get vector embedding of x
         x_emb = self.embedding(x)
@@ -78,17 +102,16 @@ class CharRNNCell(nn.Module):
         return torch.zeros(batch_size, self.num_units)
 
 
-from IPython.display import clear_output
-from random import sample
-
-char_rnn = CharRNNCell()
-# optimizer = torch.optim.Adam(char_rnn.parameters())
-history = []
-
-
 def train(lines):
+    r"""Implement RNN training loop.
+
+    Parameters
+    ----------
+    lines : list of str
+        Lines of input text.
+    """
     for i in range(1000):
-        ...
+        raise NotImplementedError('Not implemented!')
         # batch_ix = to_matrix(sample(lines, 32), max_len=MAX_LENGTH)
         # batch_ix = torch.tensor(batch_ix, dtype=torch.int64)
         #

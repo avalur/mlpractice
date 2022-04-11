7efa674147d311ea668a8473b3c67449fd0a256f
Kirill Gusev <kirill@kendze.net>
2022-04-11 23:45:19 +0300

    Added one more pre-train test and added evaluation test

--- mlpractice/tests/linear_classifier/test_linear_softmax_classifier.py
+++ mlpractice/tests/linear_classifier/test_linear_softmax_classifier.py
@@ -6,10 +6,10 @@ try:
 except ImportError:
     LinearSoftmaxClassifier = None
 
+from sklearn.metrics import accuracy_score
 from data_prep import load_and_split_data
 
 
-# pre-train tests
 def test_output_shape(data, linear_softmax_classifier=LinearSoftmaxClassifier):
     with ExceptionInterception():
         X_train, X_test, y_train, y_test = data
@@ -24,8 +24,39 @@ def test_output_shape(data, linear_softmax_classifier=LinearSoftmaxClassifier):
             "Classifier output must match the number of input samples"
 
 
+def test_increase_acc(data, linear_softmax_classifier=LinearSoftmaxClassifier):
+    with ExceptionInterception():
+        X_train, _, y_train, _ = data
+        acc_list = []
+        for num_epochs in [1, 10, 50, 100, 200, 300, 400]:
+            clf = linear_softmax_classifier()
+            clf.fit(X_train, y_train, batch_size=20, epochs=num_epochs)
+            y_pred_train = clf.predict(X_train)
+            acc_list.append(accuracy_score(y_train, y_pred_train))
+
+        assert sorted(acc_list) == acc_list, \
+            "Accuracy should increase as number of epochs increase(until some moment)"
+
+
+def test_evaluation(data, linear_softmax_classifier=LinearSoftmaxClassifier):
+    with ExceptionInterception():
+        X_train, X_test, y_train, y_test = data
+        clf = linear_softmax_classifier()
+        clf.fit(X_train, y_train, batch_size=20, epochs=400)
+        y_pred = clf.predict(X_test)
+
+        assert accuracy_score(y_test, y_pred) > 0.94, \
+            "Accuracy on test should be > 0.94"
+
+
 def test_all(linear_softmax_classifier=LinearSoftmaxClassifier):
     data = load_and_split_data()
+    data_scaled = load_and_split_data(scale=True)
+
+    # pre-train tests
     test_output_shape(data, linear_softmax_classifier)
+    test_increase_acc(data_scaled, linear_softmax_classifier)
+    # evaluation tests
+    test_evaluation(data_scaled, linear_softmax_classifier)
 
     print("All tests passed!")

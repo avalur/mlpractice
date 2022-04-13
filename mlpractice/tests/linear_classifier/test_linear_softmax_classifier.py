116dde4c9a7c2b78076112b2643fb96887044e85
Kirill Gusev <kirill@kendze.net>
2022-04-13 15:30:27 +0300

    Minor change

--- mlpractice/tests/linear_classifier/test_linear_softmax_classifier.py
+++ mlpractice/tests/linear_classifier/test_linear_softmax_classifier.py
@@ -12,15 +12,12 @@ from data_prep import load_and_split_data
 
 def test_output_shape(data, linear_softmax_classifier=LinearSoftmaxClassifier):
     with ExceptionInterception():
-        X_train, X_test, y_train, y_test = data
+        X_train, _, y_train, _ = data
         clf = linear_softmax_classifier()
         clf.fit(X_train, y_train)
         train_predict = clf.predict(X_train)
-        test_predict = clf.predict(X_test)
 
         assert train_predict.shape == (X_train.shape[0],), \
-            "Classifier output must match the number of input samples"
-        assert test_predict.shape == (X_test.shape[0],), \
             "Classifier output must match the number of input samples"
 
 

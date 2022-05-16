881706ab2b6ca73f3f588c5bd196a6d5a10684a1
Kirill Gusev <kirill@kendze.net>
2022-05-16 16:53:24 +0300

    Add data base

--- grader/__init__.py
+++ grader/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 from .gdoc import authorize_and_return_worksheet, update_ws_stats
 from ast import literal_eval
@@ -5,6 +7,15 @@ from ast import literal_eval
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'grader.sqlite'),
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route('/grade', methods=['POST', 'GET'])
     def grader():
@@ -16,5 +27,8 @@ def create_app():
             worksheet = authorize_and_return_worksheet()
             update_ws_stats(worksheet, name, stats)
         return "Grading grades"
+
+    from . import db
+    db.init_app(app)
 
     return app

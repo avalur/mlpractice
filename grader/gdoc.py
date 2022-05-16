a21ecb090630d2e594c44d9b9694f6521ab39fcb
Kirill Gusev <kirill@kendze.net>
2022-05-16 16:30:21 +0300

    Add one test to stats

--- grader/gdoc.py
+++ grader/gdoc.py
@@ -20,7 +20,7 @@ def update_ws_stats(worksheet: gspread.worksheet.Worksheet, name, stats):
         len_col_name += 1
 
     ind = names.index(name) + 3  # +2 for 'Name', '' and +1, because numeration in table starts from 1
-    cell_lists = [worksheet.range(ind, 2, ind, 6), worksheet.range(ind, 7, ind, 8)]
+    cell_lists = [worksheet.range(ind, 2, ind, 7), worksheet.range(ind, 8, ind, 9)]
     for cell_list, task in zip(cell_lists, stats.keys()):
         for cell, problem in zip(cell_list, stats[task].keys()):
             cell.value = stats[task][problem]
